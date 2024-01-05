f527040d4eef7590555ef612e5c47c6583837763
truyen.nguyentuan <truyen.nguyentuan@vti.com.vn>
2024-01-05 09:52:22 +0700

    Pageable

--- src/main/java/vn/com/dsk/demo/feature/repository/BookRepository.java
+++ src/main/java/vn/com/dsk/demo/feature/repository/BookRepository.java
@@ -1,5 +1,7 @@
 package vn.com.dsk.demo.feature.repository;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vn.com.dsk.demo.feature.model.Book;
 import vn.com.dsk.demo.feature.model.Category;
@@ -10,5 +12,9 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
     List<Book> findAllByCategory(Category category);
 
     List<Book> findAllByIsActive(Boolean isActive);
+
+    List<Book> findAllByIsActive(Boolean isActive, Pageable pageable);
+
+    List<Book> findAllByIsActive(Boolean isActive, PageRequest pageRequest);
 
 }
